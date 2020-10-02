6977216e5a7c2f69ade229951258cc011f2d2696
rrkas <rrka97wal@gmail.com>
2020-10-02 12:16:51 +0530

    page transition animation done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:moodairy/models/activity.dart';
 import 'package:moodairy/models/moodcard.dart';
 import 'package:moodairy/screens/chart.dart';
 import 'package:moodairy/screens/homepage.dart';
 import 'package:moodairy/screens/start.dart';
+import 'package:moodairy/widgets/pagetransition.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -15,33 +15,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
-    value: MoodCard(),
-    child:
-    MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      value: MoodCard(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            },
+          ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: StartPage(),
+        routes: {
+          '/home_screen': (ctx) => HomeScreen(),
+          '/chart': (ctx) => MoodChart(),
+        },
       ),
-      home: StartPage(),
-      routes: {
-
-        '/home_screen':(ctx)=>HomeScreen(),
-        '/chart':(ctx)=> MoodChart(),
-      },
-    ));
+    );
   }
 }
-
